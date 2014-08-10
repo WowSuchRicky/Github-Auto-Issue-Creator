b8e7b67b25ca913ab16d302f80e1225e64ebc51c
Ricky54326 <mutschlechne@wisc.edu>
2014-08-10 03:21:56 -0500

    Works for subdirectories now too!

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python
-from os import listdir
-
+#from os import listdir, path
+import os
 #issue class, just has the content and lineNumber fields right now.
 class Issue:
 	def __init__(self, issueContent, lineNumber):
 		self.data = []
 		self.issue = issueContent
 		self.line = lineNumber
+
+
+#Function that gets all of the files (and folders) in a folder
+def getFiles(directory):
+	#List all sub-directories and files
+	fileList = []
+
+	for d in os.listdir(directory):
+		d = directory + "/" + d #make the format actually work for our function calls
+
+		#print d, os.path.isdir(d) #debug
+
+		#if the "file" is a directory...
+		if os.path.isdir(d):
+			for file in getFiles(d): #recursively iterate through the subfolders
+				fileList.append(file)
+
+		#otherwise the file is indeed a file
+		else:
+			fileList.append(d)
+
+	#return list of actual files to open
+	return fileList
 
 
 #Function which takes a file and returns a list of Issues
@@ -16,27 +39,31 @@ def lookForIssue(file):	#reads through an input file and returns a list of issue
 	issueList = []
 
 	with open(file) as f:
-		print "Opened: ", d
+		print "Opened: ", file
 		for line in f:
 			if "TODO" in line:
 				iss = Issue(line, lineNumber)
 				issueList.append(iss)
 				lineNumber = 0;
-			lineNumber = lineNumber + 1
+			lineNumber += 1
 
 	return issueList
 
 
+def unitTest():
+	#local variables
+	issueList = []
+	files =	getFiles("test")
+	for file in files:
+		for issue in lookForIssue(file):
+			issueList.append(issue)
 
-#local variables
-issueList = []
 
-#List all sub-directories and files
-for d in listdir("test"):
-	d = "test/" + d
-	for issue in lookForIssue(d):
-		issueList.append(issue)
 
-print "\n\n\n\n ISSUES TO BE ADDED TO THE REPO:"
-for issue in issueList:
-	print issue.issue, " on line: ", issue.line
+	print "\n\n\n\n ISSUES TO BE ADDED TO THE REPO:"
+	for issue in issueList:
+		print issue.issue, " on line: ", issue.line
+
+
+
+unitTest()
