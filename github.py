c3f0e96cb03400d6515e23f428ef05a169610dc7
Ricky54326 <mutschlechne@wisc.edu>
2014-08-10 18:50:31 -0500

    Added getValue(key) and addProperty(key, value) for our settings file.

--- github.py
+++ github.py
@@ -2,7 +2,24 @@ import requests
 import json
 
 API_URL = 'https://api.github.com'
+SETTINGS = "settings.williames" #settings file
 
 
 def getToken():
-	
+
+
+def getValue(key):
+	with open(SETTINGS) as f:
+		for line in f:
+			if key in line:
+				return line.split(" ")[1]
+
+	return ""
+		
+def addProperty(key, value):
+	with open(SETTINGS, "a") as sett:
+		sett.write(key, " ", value "\n")
+		return True
+
+	return False #something bad happenesd
+
