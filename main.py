37dee45ecd73914f306cd164b57d60666604b876
Ricky54326 <mutschlechne@wisc.edu>
2014-08-10 02:45:27 -0500

    Added rudimentary working version of the parsing, no functions yet lel

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print "Hello williames"
+#!/usr/bin/python
+from os import listdir
+
+#issue class, just has the content and lineNumber fields right now.
+class Issue:
+	def __init__(self, issueContent, lineNumber):
+		self.data = []
+		self.issue = issueContent
+		self.line = lineNumber
+
+#local variables:
+issueList = []
+lineNumber = 1
+
+#List all sub-directories and files
+iss = Issue("", "")
+for d in listdir("test"):
+	d = "test/" + d
+	with open(d) as f:
+		print "Opened: ", d
+		for line in f:
+			if "TODO" in line:
+				iss = Issue(line, lineNumber)
+				issueList.append(iss)
+				lineNumber = 0;
+			lineNumber = lineNumber + 1
+
+print "\n\n\n\n ISSUES TO BE ADDED TO THE REPO:"
+for issue in issueList:
+	print issue.issue, " on line: ", issue.line
